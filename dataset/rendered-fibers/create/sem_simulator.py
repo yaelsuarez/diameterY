e48b9ad8237b711c88a45422f797f60397a61e73
fcossio <fer_cossio@hotmail.com>
2022-07-15 23:53:58 +0200

    create dataset medium

--- dataset/rendered-fibers/create/sem_simulator.py
+++ dataset/rendered-fibers/create/sem_simulator.py
@@ -26,7 +26,7 @@ def create_rendered_dataset(sizes:Tuple[int, int, int], **kwargs):
     """
     with wandb.init(project='diameterY', job_type='create-data', mode='online') as run:
         raw_data = wandb.Artifact(
-            'rendered-fibers-mini', type='dataset',
+            'rendered-fibers-medium', type='dataset',
             description='Single straight fibers split into train/val/test',
             metadata={
                 'train_val_test':sizes,
@@ -44,32 +44,13 @@ def create_rendered_dataset(sizes:Tuple[int, int, int], **kwargs):
                         succeed=1
                     except Exception as e:
                         print(e)
-                # --- load the image to log it to WandB
-                im = Image.open(f'output/{task_uid}.png')
-                seg = np.load(f'output/{task_uid}_seg.npz')['y']
-                # with open(f'output/{task_uid}.json') as file:
-                #     task_data = json.load(file)
-                # table = wandb.Table(data=pd.DataFrame(gen_table(task_data)))
-                # wandb_im = wandb.Image(im, caption=task_uid, masks={
-                #     'ground_truth':{
-                #         'mask_data':np.squeeze(seg),
-                #         'class_labels':{int(x):f'f{int(x-100):02d}' for x in task_data.keys()}    
-                #     }
-                # })
-                # raw_data.add(wandb_im, name=task_uid)
-                # raw_data.add(table, name=f'{task_uid}_params')
-                raw_data.add_file(f'output/{task_uid}.png', name=task_uid)
-                raw_data.add_file(f'output/{task_uid}_seg.npz', name=f'{task_uid}_seg')
+                # # --- load the image to log it to WandB
+                # im = Image.open(f'output/{task_uid}.png')
+                # seg = np.load(f'output/{task_uid}_seg.npz')['y']
+                
+                raw_data.add_file(f'output/{task_uid}.png', name=f'{task_uid}.png')
+                raw_data.add_file(f'output/{task_uid}_seg.npz', name=f'{task_uid}_seg.npz')
                 raw_data.add_file(f'output/{task_uid}.json', name=f'{task_uid}_params')
         run.log_artifact(raw_data)
 if __name__ == "__main__":
-    create_rendered_dataset([256,64,64])
-
-# for i in tqdm(range(10)):
-    
-#     task_uid = f'{i:04d}'
-#     cmd = f'docker run --rm -i -u $(id -u):$(id -g) --volume "$(pwd):/kubric" kubruntu_sdf /usr/bin/python3 "create/sem_worker.py" {task_uid}'
-#     subprocess.run(cmd, shell=True, check=True)
-
-#     import numpy as np
-#     seg = np.load(f'output/{task_uid}_seg.npz')['y']
+    create_rendered_dataset([2048,256,256])
